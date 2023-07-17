063ad3c9c77a853ee49972160e9c01063a9d581e
sajc30 <sajandeepcheema@gmail.com>
2023-07-17 19:20:22 -0400

    Final changes

--- final_project/machinetranslation/tests.py
+++ final_project/machinetranslation/tests.py
@@ -6,16 +6,16 @@ class TestMyModule(unittest.TestCase):
     def test_english_to_french(self):
         self.assertEqual(english_to_french('hello'),'bonjour')
         self.assertEqual(english_to_french('goodbye'),'au revoir')
-        # Test None returns empty string
-        self.assertNotEqual(english_to_french("None"), '')
+        self.assertNotEqual(english_to_french('hello'),'hello')
+        
         
 
 class TestMyModuleFrToEng(unittest.TestCase):
     def test_french_to_english(self):
         self.assertEqual(french_to_english('bonjour'),'hello')
         self.assertEqual(french_to_english('au revoir'),'goodbye')
-        # Test None returns empty string
-        self.assertNotEqual(french_to_english("None"), '')
+        self.assertNotEqual(french_to_english('bonjour'),'bonjour')
+        
         
         
 
